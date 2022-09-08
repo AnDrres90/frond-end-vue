bb2f852e753a28a3502d90a44f27bb6c3ae63918
AnDrres90 <marlon.gomez0264@misena.edu.co>
2022-09-08 08:26:30 -0500

    new

--- src/components/myLogin.vue
+++ src/components/myLogin.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
-        <img src="@/assets/fond.jpg" alt="fondo" class="position-absolute" style="width: 100%; height: 100%; opacity: 0.4;">
+        <img src="@/assets/fond.jpg" alt="fondo" class="position-absolute"
+            style="width: 100%; height: 100%; opacity: 0.4;">
     </div>
     <div class="col-md-4 offset-md-4 container p-5">
         <form @submit.prevent="FindUser()" class="card card-body form-group">
-            <h1 class="text-center h2 p-5">SIGN IN</h1>
+            <h1 class="text-center h2 p-5">SIGN IN <img src="@/assets/log-in.png" alt="signin" class="imgform "></h1>
             <input type="email" placeholder="email" v-model="user.email" class="form-control mb-3">
             <input type="password" placeholder="password" v-model="user.password" class="form-control mb-3">
             <button class="btn btn-primary">Send</button>
             <p class="text-center h6 m-3" style="color: #ea5959;">{{msg}}</p>
-            <p class="p-3 text-center">Don't have an account <router-link to="/logup">Sign up</router-link> </p>
+            <p class="p-3 text-center">Don't have an account <router-link to="/logup">Sign up</router-link>
+            </p>
         </form>
     </div>
 </template>
@@ -27,7 +29,7 @@ export default defineComponent({
             user: {} as User
         };
     },
-    async mounted(){
+    async mounted() {
         localStorage.removeItem('token');
     },
     methods: {
@@ -37,17 +39,23 @@ export default defineComponent({
                 'password': this.user.password
             }
             await signInUser(json)
-            .catch(err => {
-                this.msg = err.response.data.message;
-            }); 
+                .catch(err => {
+                    this.msg = err.response.data.message;
+                });
             const res = await signInUser(json)
             const token = res.data.token;
             axios.defaults.headers.common['Authorization'] = token;
             localStorage.setItem('token', token);
-            if(token){
+            if (token) {
                 await this.$router.push('/profileuser')
             }
         }
     }
 });
 </script>
+
+<style>
+.imgform {
+    width: 40px;
+}
+</style>
--- src/components/myLogup.vue
+++ src/components/myLogup.vue
@@ -1,17 +1,19 @@
 <template>
     <div>
-        <img src="@/assets/fond.jpg" alt="fondo" class="position-absolute" style="width: 100%; height: 100%; opacity: 0.4;">
+        <img src="@/assets/fond.jpg" alt="fondo" class="position-absolute"
+            style="width: 100%; height: 100%; opacity: 0.4;">
     </div>
     <div class="col-md-4 offset-md-4 container p-5">
         <form @submit.prevent="SaveUser()" class="card card-body form-group">
-            <h1 class="text-center h2 p-5">SIGN UP</h1>
+            <h1 class="text-center h2 p-5">SIGN UP <img src="@/assets/sign-up.png" alt="signup" class="imgform "></h1>
             <input type="text" placeholder="name" v-model="user.name" class="form-control mb-3">
             <input type="text" placeholder="last name" v-model="user.lastName" class="form-control mb-3">
             <input type="email" placeholder="email" v-model="user.email" class="form-control mb-3">
             <input type="password" placeholder="password" v-model="user.password" class="form-control mb-3">
             <button class="btn btn-primary">Send</button>
             <p class="text-center h6 m-3" style="color: #ea5959;">{{msg}}</p>
-            <p class="p-3 text-center">I have an account <router-link to="/">Sign in</router-link> </p>
+            <p class="p-3 text-center">I have an account <router-link to="/">Sign in</router-link>
+            </p>
         </form>
     </div>
 </template>
@@ -32,12 +34,18 @@ export default defineComponent({
         async SaveUser() {
             this.$router.push('/');
             await createUser(this.user)
-            .catch(err => {
-                this.msg = err.response.data.message;
-            });
+                .catch(err => {
+                    this.msg = err.response.data.message;
+                });
             await createUser(this.user);
         }
     }
 });
 
 </script>
+
+<style>
+.imgform {
+    width: 40px;
+}
+</style>
